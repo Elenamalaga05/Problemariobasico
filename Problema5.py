caa86a0f60f565bc914c61d10e0dcc2e1d735b9e
Elenamalaga05 <carmenmalaga177@gmail.com>
2024-12-11 06:43:07 -0600

    Update Problema5.py

--- Problema5.py
+++ Problema5.py
@@ -21,3 +21,24 @@ fun main() {
         println("Entrada inválida. Por favor, ingresa valores numéricos positivos.")
     }
 }
+
+
+#Caso con antigüedad menor a 5 años:
+
+Ingresa el sueldo base del empleado: 1000
+Ingresa los años de antigüedad del empleado: 3
+El sueldo a cobrar es: $1300.00
+
+
+#Caso con antigüedad de 5 años o más:
+
+Ingresa el sueldo base del empleado: 1200
+Ingresa los años de antigüedad del empleado: 7
+El sueldo a cobrar es: $1800.00
+
+
+#Entrada inválida:
+
+Ingresa el sueldo base del empleado: abc
+Ingresa los años de antigüedad del empleado: -3
+Entrada inválida. Por favor, ingresa valores numéricos positivos.
